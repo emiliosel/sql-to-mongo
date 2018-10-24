3fa0a93b1ea2fa3521b5d4b02fb2c34faa9db1cf
Aimilios Selmanaj <aselmanaj@webjar.me>
2018-10-24 03:12:22 +0300

    Started developing the module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,40 @@ function sayHiTo(name) {
   return `Hi, ${name}`;
 }
 
+function migrate(config) {
+  const {
+    table,
+  } = config;
+
+  return table;
+}
+
+migrate({
+  fromTable: 'articles',
+  toCollection: 'articles',
+  columns: {
+    oldId: {
+      type: String,
+      from: 'id',
+      embed: {
+        type: Object,
+        fromTable: 'categories',
+        columns: {
+          name: {
+            type: String,
+            from: 'title',
+          },
+        },
+      },
+    },
+  },
+});
 const message = sayHiTo('Bruno');
 
 console.log(message);
+
+export default function (config) {
+  return {
+    config,
+  };
+}
